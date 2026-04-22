be43ec31ed104ae575c9d14ab6255eb5c636fc5b
Kurama622 <kurama174@163.com>
2026-04-22 20:35:35 +0800

    chore: update log title

--- lua/fsbuffer/log.lua
+++ lua/fsbuffer/log.lua
@@ -1,8 +1,8 @@
 local log = {}
 function log.error(msg)
-  vim.notify(msg, vim.log.levels.ERROR, { title = "fsbuffer" })
+  vim.notify(msg, vim.log.levels.ERROR, { title = "Fsbuffer" })
 end
 function log.warn(msg)
-  vim.notify(msg, vim.log.levels.WARN, { title = "fsbuffer" })
+  vim.notify(msg, vim.log.levels.WARN, { title = "Fsbuffer" })
 end
 return log
